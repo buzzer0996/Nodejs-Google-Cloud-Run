b2015c9429637e291767fa65d7264a0a07de2ba2
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2025-09-09 12:12:08 +0530

    Create variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,21 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "service_name" {
+  description = "Cloud Run Service Name"
+  type        = string
+  default     = "flaskapp"
+}
+
+variable "container_image" {
+  description = "Container Image URI"
+  type        = string
+}
